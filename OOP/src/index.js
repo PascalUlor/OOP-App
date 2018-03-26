95f64a2cc1448fa935c0fc1b789ee15cc91c1a4b
Pascal Ulor <pascalulor@yahoo.com>
2018-03-26 22:57:21 +0100

    Script edit

--- OOP/src/index.js
+++ OOP/src/index.js
@@ -10,25 +10,25 @@ export class Population {
     get name() {
         return this.name;
     }
+}
 
-    export class Citizen extends Population {
-        constructor(age = 21, stateOfOrigin = Imo, status = single) {
-            super(this.name);
-            this.age = age;
-            this.stateOfOrigin = stateOfOrigin;
-            this.status = status;
-        }
+export class Citizen extends Population {
+    constructor(age = 21, stateOfOrigin = Imo, status = single) {
+        super(this.name);
+        this.age = age;
+        this.stateOfOrigin = stateOfOrigin;
+        this.status = status;
+    }
 
-        getBio() {
-            console.log(`${this.name} is ${this.age} from ${this.stateOfOrigin} and is ${this.status}. ${regVoter}`)
-        }
+    getBio() {
+        console.log(`${this.name} is ${this.age} from ${this.stateOfOrigin} and is ${this.status}. ${regVoter}`)
+    }
 
-        regVoter() {
-            if (this.age >= 18) {
-                return "You are old enough to vote";
-            } else {
-                return "You are not old enough to vote"
-            }
+    regVoter() {
+        if (this.age >= 18) {
+            return "You are old enough to vote";
+        } else {
+            return "You are not old enough to vote"
         }
     }
 }
